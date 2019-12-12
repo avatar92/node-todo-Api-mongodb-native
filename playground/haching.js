6c182d51dadab8e75d721c68984f27148287cf02
avatar92 <adelbensalem1992@gmail.com>
2019-12-12 14:45:57 +0100

    add hashing password before saving in the mongoDB data base

--- playground/haching.js
+++ playground/haching.js
@@ -1,16 +1,20 @@
 const {SHA256}=require('crypto-js'); 
 const jwt=require('jsonwebtoken')
+const bcrypt=require('bcryptjs');
 
+var password='123abc!';
 
-var data={
-  id:10
-}
+bcrypt.genSalt(10,(err,salt)=>{
+  bcrypt.hash(password,salt,(err,hash)=>{
+    console.log(hash)
+  })
+});
 
-var token=jwt.sign(data,'123abc')
-console.log(token)
+var hashedPassword='$2a$10$OAsp9XxSYOtjMpswriIadegnx34egNTVGSKbQlg0tYuV8YEnG8Zm6';
 
-var decoded=jwt.verify(token,'123abc')
-console.log('decoded:',decoded)
+bcrypt.compare('123',hashedPassword,(err,res)=>{
+  console.log(res)
+});
 
 // var message='I am user number 3'
 
